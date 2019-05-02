130e9a9fa289d2226c32f4f7a068b78cd45acabe
hym <dragoncat0815@gmail.com>
2019-05-03 00:44:17 +0800

    linebot+google sheet api

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const configGet = require('config');
 const app = express();
 const mysql = require("mysql");
 const moment = require('moment');
+const google = require('googleapis');
+const googleAuth = require('google-auth-library');
+// const authentication = require("./authentication");
 
 const db = mysql.createConnection({
     host: "medical.cg1fvo9lgals.ap-southeast-1.rds.amazonaws.com",
@@ -25,6 +28,7 @@ const bot = linebot({
         "3xy2S0/lj5fXOC5LNR7Ly86ettyCR0ohfNgB7f6G6JoxA45dfE4dbOL+KvIBikANeCqidzdXCAqQBJknsiM8wp8HPH5tfFL5grAJxgsB7RQWu22iuBu0VrHUzwdk2zx7sXpmZ5GkxgEH88k5mhhTNQdB04t89/1O/w1cDnyilFU="
 });
 
+// const myClientSecret = 請將client_secret.json檔案的內容放在這裡，前後不能加引號
 const linebotParser = bot.parser();
 
 app.post('/webhook', linebotParser, function (req, res) {
@@ -33,6 +37,7 @@ app.post('/webhook', linebotParser, function (req, res) {
 
 var appointment_time = [];
 var search_time = [];
+const new_user = [];
 
 bot.on('message', function (event) {
     var userinput = event.message.text;
@@ -43,19 +48,23 @@ bot.on('message', function (event) {
         console.log(appointment_time);
         event.reply('請問要掛哪一科？');
     } else if (userinput == '牙科' || userinput == '胸腔科') {
-        event.reply('請輸入身分證字號');
+        event.reply('請輸入身分證字號以進行掛號:');
         appointment_time.push(event.timestamp);
         console.log(appointment_time.length);
     } else if (appointment_time.length >= 2 && userinput.match(id_regex)) {
         db.query("SELECT * FROM `med_appointment_sub` WHERE `ID`= ?", [userinput], (error, results, fields) => {
             if (results.length == true) {
-                console.log([{ type: 'text', text: '掛號資訊:' }, { type: 'text', text: '醫生:' + results[0].Doctor }, { type: 'text', text: '科別:' + results[0].Subject }, { type: 'text', text: '看診時間:' + moment(results[0].App_time).format('YYYY/MM/DD hh:mm') }]);
-                event.reply([{ type: 'text', text: '掛號資訊:' }, { type: 'text', text: '醫生:' + results[0].Doctor }, { type: 'text', text: '科別:' + results[0].Subject }, { type: 'text', text: '看診時間:' + moment(results[0].App_time).format('YYYY/MM/DD hh:mm') }]);
-            } else {
-                var new_user = []
-                // var i;
+                event.reply({ type: 'text', text: '你有已掛號的紀錄，請輸入查詢以檢視相關資訊' });
+            } else if (results.length == 0) {
+                // userinput = '';
+                event.reply('目前沒有你的掛號紀錄，請輸入:ID, Name, Sex, Subject, App_time, Doctor來掛號');
+                console.log('new_user.length = ' + new_user.length);
+                new_user.push(userinput);
+                console.log(userinput)
+                userinput.split(',');
+
                 const val = {
-                    ID: new_user[0],
+                    Id: new_user[0],
                     Name: new_user[1],
                     Sex: new_user[2],
                     Subject: new_user[3],
@@ -63,23 +72,38 @@ bot.on('message', function (event) {
                     Doctor: new_user[5]
                 };
 
-                event.reply('請輸入: 生份證字號, Name, Sex, Subject, App_time, Doctor');
-                if (new_user.length <= 6) {
-                    new_user.push(userinput);
-                    console.log(new_user.length);
-                }
-                console.log(val);
+                // if (new_user.length < 6 && results.length == false) {
+                //     event.reply('請輸入ID: ');
+                //     new_user.push(userinput)
+
+                //     console.log(new_user);
+                //     for (i = 0; i < 6; i++) {
+                //         if (i < 6) {
+                //             new_user.push(userinput);
+                //         }
+                //         console.log(new_user);
+                //     }
+                // }
+                console.log('valID = ' + val.Id);
+                console.log('valName = ' + val.Name);
+                console.log('valSex = ' + val.Sex);
+                console.log('valSubject = ' + val.Subject);
+                console.log('valApp_time = ' + val.App_time);
+                console.log('valDoctor = ' + val.Doctor);
 
                 // db.query('insert into `med_appointment_sub` set ?', val, (error, results, fields) => {
                 //     console.log(results)
                 // });
             }
+            // console.log('new_user = ' + new_user);
+            // new_user.push(userinput);
+            console.log('new_user = ' + new_user);
         });
         appointment_time = [];
         console.log(appointment_time.length);
-    } else if (!userinput.match(id_regex)) {
-        console.log('no id value');
-        event.reply('no id value');
+    } else if (!userinput.match(id_regex) && new_user == null) {
+        console.log('wrong id value');
+        event.reply('wrong id value');
     }
 
     if (userinput == "查詢") {
